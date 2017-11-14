0d5e759766567335f7139be387a8e65adf1b02c9
Alove13 <aaronlove.333@gmail.com>
2017-11-14 04:37:13 +0000

    Setting up backend

--- creative/public/javascripts/recipeClient.js
+++ creative/public/javascripts/recipeClient.js
@@ -1,14 +1,40 @@
 angular.module('recipeApp', [])
-.controller('recipeCtrl', 
-['$scope', 
-function($scope){
- $scope.test = "Leggo my Eggo"
+.factory('recipeFetcher', recipeFetcher)
+.controller('recipeCtrl', recipeCtrl)
+function recipeFetcher ($http) {
+ var API_ROOT = 'savedRecipes'
+ return {
+  get: function() {
+   return $http
+   .get(API_ROOT)
+   .then(function(response){
+    return response.data
+   })
+  },
+
+  tryit: function() {
+   var recipeUrl = "/savedRecipes"
+   return $http
+    .get(recipeUrl)
+    .then(function(response) {
+     console.log("get worked!")
+     console.log("data: ", data)
+     return response.data    
+    })
+  }
+ 
+ }//all returned stuff
+}//end of factory
+
+function recipeCtrl ($scope, recipeFetcher) {
+ $scope.test = "Made with Love, by Love"
  
  $scope.pastSearches = []//this will be stored in Mongo to help for future searches
  $scope.savedRecipes = []//this object will be sent to server to be added to mongo to remember foreva!
 
  $scope.updatePastSearches = function() {
   $scope.pastSearches.push({search: $scope.inputRecipe}) //will be wiped unless send to DB
-  console.log($scope.inputRecipe.search)
- } 
-}])
+  //  console.log($scope.inputRecipe.search)
+ }
+
+}
--- creative/routes/index.js
+++ creative/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require('express')
+var router = express.Router()
+var request = require('request')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
 });
 
+router.get('/savedRecipes', function(req, res){
+ var recipeUrl = "http://food2fork.com/api/search?key=eb2b238428ee5a9eea38395e0c399aca&q="
+ console.log("in savedRecipes")  
+ console.log("recipeUrl: ", recipeUrl)
+ console.log("req.query.q: ", req.query.q)
+ recipeUrl += req.query.q
+ request(recipeUrl).pipe(res)//takes care of CORS issue 
+})
 module.exports = router;
